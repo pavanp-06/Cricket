52ac3c0ea03e2bbd6beede95699af65ac7bd6773
pavanp-06 <100146710+pavanp-06@users.noreply.github.com>
2022-10-31 23:07:53 +0530

    Update Abhi.py

--- Abhi.py
+++ Abhi.py
@@ -3,6 +3,4 @@ import csv
 T1=input("Enter the first team")
 T2=input("Enter the second team")
 import random
-x=random.choicesrandom([T1,T2])
-if x==T1:
-    print("Hello")
+x=random.choices([T1,T2])
