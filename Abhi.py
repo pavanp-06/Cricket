a0b918d9d3041ba8a3f4faa40fb1929328568a65
pavanp-06 <100146710+pavanp-06@users.noreply.github.com>
2022-10-31 18:04:59 +0530

    Revert "Update Abhi.py"
    This reverts commit 699627ed75c9dca61e85245f6ed4da183106cf8b.

--- Abhi.py
+++ Abhi.py
@@ -1,6 +1,16 @@
-#cricket
-import csv
-T1=input("Enter the First team")
-T2=input("Enter the second team")
-with open("Score.csv",'w',newline=" ")as f:
-    x=random
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# hi pavan 
