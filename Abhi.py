699627ed75c9dca61e85245f6ed4da183106cf8b
pavanp-06 <100146710+pavanp-06@users.noreply.github.com>
2022-10-31 17:46:05 +0530

    Update Abhi.py

--- Abhi.py
+++ Abhi.py
@@ -1,16 +1,6 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# hi pavan 
+#cricket
+import csv
+T1=input("Enter the First team")
+T2=input("Enter the second team")
+with open("Score.csv",'w',newline=" ")as f:
+    x=random
