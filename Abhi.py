787e8d94ebd78963c3012b7140a5338a2328de03
pavanp-06 <100146710+pavanp-06@users.noreply.github.com>
2022-11-02 09:53:09 +0530

    Update Abhi.py

--- Abhi.py
+++ Abhi.py
@@ -1,6 +1,16 @@
 #Cricket
 import csv
-T1=input("Enter the first team")
-T2=input("Enter the second team")
+T1=input("Enter the name of the first team")
+T2=input("Enter the name of the second team")
+q=int(input("Enter the no. of players in each team"))
+t1=[]
+t2=[]
+print("Enter the player in",T1"Team")
+for i in range(q):
+    K=input("Enter the name of the playerEnter the suffix of (C) and (WK) for captain and wicket-keeper respectively")
+    t1.append(K)
+for i in range(q):
+    L=input("Enter the name of the playerEnter the suffix of (C) and (WK) for captain and wicket-keeper respectively")
+    t2.append(L)
 import random
 x=random.choices([T1,T2])
